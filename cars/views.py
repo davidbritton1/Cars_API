901dc15307f621d9e8da1e0906e3b05eec3c60fd
davidbritton1 <102608670+davidbritton1@users.noreply.github.com>
2022-04-13 13:29:17 -0700

    Updated project URLs + Created first view function + Created Cars App URL File

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+import imp
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def cars_list(request):
+
+    return Response('ok')
+    
